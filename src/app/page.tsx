a2cb53c55c59110a4b41c2dea0902b09dfe31ef1
Conor <chepenikconor@gmail.com>
2024-04-03 23:43:53 -0400

    trying to fix home image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       {/* Apollo Image Container */}
       <div className="relative w-full h-0 pb-[56.25%]">
         <Image
-          src="/Apollo.png"
+          src="https://i.nostr.build/LeVgQ.png"
           alt="Apollo"
           layout="fill"
           objectFit="contain"
