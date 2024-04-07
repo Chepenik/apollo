f5f13a17dd7d82519eb01daaaba3127c73fa449d
Conor <chepenikconor@gmail.com>
2024-04-07 18:45:55 -0400

    mobile optimization

--- src/app/abstract/page.tsx
+++ src/app/abstract/page.tsx
@@ -7,44 +7,52 @@ export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="relative min-h-screen bg-[#301f13] overflow-hidden flex flex-col justify-center items-center">
-      {/* Full-screen background */}
+    <div className="relative min-h-screen bg-[#301f13] overflow-hidden flex flex-col justify-center items-center md:px-4 sm:px-2">
       <div className="flex flex-col items-center justify-center">
-        {/* Adjusted to center the logo and added margin bottom */}
         <Image
           src="/Apollo transparent.png"
           alt="Apollo"
-          width={200}
-          height={200}
-          className="mb-6"
+          width={240}
+          height={240}
+          className="mb-4 md:mb-6"
         />
-        <div className="text-center text-white max-w-2xl mx-auto">
-          {/* Added max-width and auto margins for horizontal centering */}
-          <p className="mb-4">
-            Abstract. 95% of shod humans currently wear shoes that literally deform, disable and
-            damage their feet. Simply look around and you can confirm this to be true. With that
-            fact considered, it&apos;s unsurprising that roughly 75% of people develop foot problems at
-            some point in their lives. This paper seeks to upgrade our collective understanding of
-            human feet and presents a radical approach to preventing and resolving foot
-            dysfunction. The framework is grounded in first principles and serves as an effective
-            alternative to the current status quo which treats symptoms of dysfunction without
-            addressing the root cause. A paradigm shift is currently underway in how we think of
-            health broadly and foot health specifically. Instead of simply diagnosing problems and
-            treating symptoms (disease care), professionals are beginning to help their patients and
-            clients take better care of themselves (healthcare). This shift is a much needed change
-            to help us move towards better health. Optimal health is the output of exposing our
-            bodies to natural behaviours that align with our biology. Within the domain of foot
-            health, it is the duty of care and responsibility of health professionals to protect
-            those they serve from the harmful effects of unnatural footwear. By increasing
-            awareness, respecting first principles, focusing on the essentials and designing aligned
-            incentives, we have an opportunity to prevent and resolve the vast majority of foot
-            dysfunction.
+        <div className="text-justify text-white max-w-2xl mx-auto px-4 sm:px-2">
+          <p className="mb-4"><span className='font-bold'>Abstract: </span>
+             95% of humans currently wear shoes that literally
+            deform, disable and damage their feet. Simply look around and you
+            can confirm this to be true. With that fact considered, it&apos;s
+            unsurprising that roughly 75% of people develop foot problems at
+            some point in their lives. This paper seeks to upgrade our
+            collective understanding of human feet and presents a radical
+            approach to preventing and resolving foot dysfunction. The framework
+            is grounded in first principles and serves as an effective
+            alternative to the current status quo which treats symptoms of
+            dysfunction without addressing the root cause. A paradigm shift is
+            currently underway in how we think of health broadly and foot health
+            specifically. Instead of simply diagnosing problems and treating
+            symptoms (disease care), professionals are beginning to help their
+            patients and clients take better care of themselves (healthcare).
+            This shift is a much needed change to help us move towards better
+            health. Optimal health is the output of exposing our bodies to
+            natural behaviours that align with our biology. Within the domain of
+            foot health, it is the duty of care and responsibility of health
+            professionals to protect those they serve from the harmful effects
+            of unnatural footwear. By increasing awareness, respecting first
+            principles, focusing on the essentials and designing aligned
+            incentives, we have an opportunity to prevent and resolve the vast
+            majority of foot dysfunction.
+            <br />
+            <br />
+            <span className='font-bold'>[v 1.0_840,000]</span>
           </p>
-          <p className="mb-4">[v 1.0_840,000 ]</p>
+          <p className="mb-4"></p>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-center space-x-32">
-        <Link href="https://primal.net/p/npub1pegapre6vcqs22a9ccrq730gmutz2ee40ytwslt7lna8w0uw4cjsumgv6t" target="_blank">
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-32 w-full  mb-4">
+        <Link
+          href="https://primal.net/p/npub1pegapre6vcqs22a9ccrq730gmutz2ee40ytwslt7lna8w0uw4cjsumgv6t"
+          target="_blank"
+        >
           <p className="text-white">NOSTR</p>
         </Link>
         <Link href="/contact">
--- src/app/donors/page.tsx
+++ src/app/donors/page.tsx
@@ -21,54 +21,35 @@ export default function Home() {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <div className="relative h-screen bg-[#301f13] flex justify-center items-center">
-      <div className="absolute inset-0 flex flex-col items-center justify-center z-10">
+    <div className="relative min-h-screen bg-[#301f13] flex justify-center items-center">
+      <div className="flex flex-col items-center justify-center z-10 w-full px-4">
         <h2 className="text-white text-3xl mb-8">Top donors</h2>
-
         {/* Grid of donor logos and names */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-8">
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mb-8">
           {donors.map((donor, index) => (
-            <div
-              key={index}
-              className="bg-white rounded-xl overflow-hidden flex flex-col items-center justify-center p-4"
-            >
+            <div key={index} className="bg-white rounded-xl overflow-hidden flex flex-col items-center justify-center p-4">
               <Link href={donor.link ?? ''} target="_blank">
-                <Image
-                  src={donor.imageUrl}
-                  alt={`${donor.name} Logo`}
-                  width={80}
-                  height={80}
-                  className="object-contain"
-                />
-                <p className="text-center text-sm text-gray-600 mt-2">
-                  {donor.name}
-                </p>
-              </Link> 
+                <Image src={donor.imageUrl} alt={`${donor.name} Logo`} width={100} height={100} className="object-contain" />
+                <p className="text-center text-sm text-gray-600 mt-2">{donor.name}</p>
+              </Link>
             </div>
           ))}
         </div>
-
         {/* Navigation buttons */}
-        <div className="flex flex-row items-center justify-center space-x-8 w-full">
+        <div className="flex flex-col sm:flex-row items-center justify-center space-x-0 space-y-4 sm:space-y-0 sm:space-x-8 mb-4">
           <Link href="https://primal.net/p/npub1pegapre6vcqs22a9ccrq730gmutz2ee40ytwslt7lna8w0uw4cjsumgv6t" target="_blank">
             <p className="text-white">NOSTR</p>
           </Link>
           <Link href="/abstract">
-            <p
-              className={`enter-button text-white px-4 py-2 rounded-md transition-colors duration-300 ${
-                isHovered ? '' : ''
-              }`}
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
+            <p className={`enter-button text-white px-4 py-2 rounded-md transition-colors duration-300 ${isHovered ? 'transparent' : 'transparent'}`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
               Continue
             </p>
           </Link>
           <Link href="/tip">
             <p className="text-white flex items-center">
               Support with Sats{' '}
-              <span className="ml-2 text-yellow-400">&#9889;</span>{' '}
-              <span className="ml-2 text-orange-500">&#x20BF;</span>
+              <span className="ml-2 text-yellow-400">⚡</span>{' '}
+              <span className="ml-2 text-orange-500">₿</span>
             </p>
           </Link>
         </div>
