70ede9c71df93ff3c88b85cfdf5db0322434b999
Conor <chepenikconor@gmail.com>
2024-04-16 11:39:02 -0400

    updated abstract

--- src/app/abstract/page.tsx
+++ src/app/abstract/page.tsx
@@ -9,24 +9,18 @@ export default function Home() {
   return (
     <div className="relative min-h-screen bg-[#301f13] overflow-hidden flex flex-col justify-center items-center md:px-4 sm:px-2">
       <div className="flex flex-col items-center justify-center">
-        <Image
-          src="/Apollo transparent.png"
-          alt="Apollo"
-          width={240}
-          height={240}
-          className="mb-4 md:mb-6"
-        />
-        <div className="text-justify text-white max-w-2xl mx-auto px-4 sm:px-2">
-          <p className="mb-4"><span className='font-bold'>Abstract: </span>
-          A simple and effective lower extremity health protocol would allow individuals to prevent and resolve injury without relying on disease care professionals who are trained to offer palliative interventions. By lowering time preference, individuals can view pain as important feedback and become capable of making lifestyle choices today that lead to a healthier tomorrow. By understanding how our biology adapts, individuals become empowered to achieve better health by reducing exposure to unnatural inputs and increasing exposure to natural inputs. Protocol consensus is achieved through peer validation and primitives could be created to offer templates for application. Disease care providers who wish to become health professionals can integrate the protocol into their approach in order to deliver effective guidance to those they serve.  We propose a solution to the problem with effective peer validated proof of work behavioural software grounded in first principles that can be easily applied by any human. This protocol seeks to upgrade our collective understanding of human health and presents a radical approach for preventing and resolving lower extremity dysfunction. We assert that the ultimate form of healthcare occurs when individuals accept responsibility for understanding fundamentals laws of biology and applying essential principles in their daily lives. In order to reclaim responsibility for health, individuals require truthful and effective behavioural software which they can apply and validate experientially. Through peer validation, protocol effectiveness will create social and financial incentives for the development of products and services that support successful application. Each individual will have their own optimal application of the protocol but the fundamentals must be respected in order to achieve effectiveness.
-            <br />
-            <br />
-            <span className='font-bold'>[v 1.0_840,000]</span>
+        <h1 className='text-white text-2xl m-8'>Apollo: A Radical Lower Extremity Health Protocol</h1>
+        <span className='font-bold text-gray-500'>[v 1.0_840,000]</span>
+        {/* <Image src="/Apollo transparent.png" alt="Apollo" width={240} height={240} className="mb-4 md:mb-6" /> */}
+        <p className='text-white my-8'>Created by Bitcoiners</p>
+        <div className="text-justify text-white max-w-4xl mx-auto px-4 sm:px-2">
+          <p className="mb-4">
+            <span className='font-bold'>Abstract: </span> A simple and effective lower extremity health protocol would allow individuals to prevent and resolve injury without relying on disease care professionals who are trained to offer palliative interventions. By lowering time preference, individuals can view pain as important feedback and become capable of making lifestyle choices today that lead to a healthier tomorrow. By understanding how our biology adapts, individuals become empowered to achieve better health by reducing exposure to unnatural inputs and increasing exposure to natural inputs. Protocol consensus is achieved through peer validation and primitives could be created to offer templates for application. Disease care providers who wish to become health professionals can integrate the protocol into their approach in order to deliver effective guidance to those they serve. We propose a solution to the problem with effective peer validated proof of work behavioural software grounded in first principles that can be easily applied by any human. This protocol seeks to upgrade our collective understanding of human health and presents a radical approach for preventing and resolving lower extremity dysfunction. We assert that the ultimate form of healthcare occurs when individuals accept responsibility for understanding fundamentals laws of biology and applying essential principles in their daily lives. In order to reclaim responsibility for health, individuals require truthful and effective behavioural software which they can apply and validate experientially. Through peer validation, protocol effectiveness will create social and financial incentives for the development of products and services that support successful application. Each individual will have their own optimal application of the protocol but the fundamentals must be respected in order to achieve effectiveness.
           </p>
           <p className="mb-4"></p>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-32 w-full mb-4">
+      <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-32 w-full mb-4 ml-0 md:ml-24">
         <Link
           href="https://primal.net/p/npub1pegapre6vcqs22a9ccrq730gmutz2ee40ytwslt7lna8w0uw4cjsumgv6t"
           target="_blank"
