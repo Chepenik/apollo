6b080476a5408ebeacba94f46bf57b2f55d7c0d6
Conor <chepenikconor@gmail.com>
2024-05-30 12:02:49 -0400

    lets see if this fixes anything lol

--- src/app/endorse/page.tsx
+++ src/app/endorse/page.tsx
@@ -1,23 +1,10 @@
-"use client"
+'use client';
+import React, { useState } from 'react';
 
-import React, { useState, useEffect, useRef } from 'react';
-
-const ContactSection: React.FC = () => {
-  const hasLogged = useRef(false);
-
-  useEffect(() => {
-    if (!hasLogged.current) {
-      console.log('Want a website like this? Visit https://magicwebstore.xyz/index.html?pubkey=02b1a6c208420d3eb5a625aa8c79689e1dd4ea94f82286d06e1df7a05e2c3a482f&relays=%5B%22wss://nostrue.com%22,%22wss://relayable.org%22,%22wss://nostr.oxtr.dev%22%5D');
-      hasLogged.current = true;
-    }
-  }, []);
-
+const FeedbackPage: React.FC = () => {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [profession, setProfession] = useState('');
-  const [credentials, setCredentials] = useState('');
-  const [company, setCompany] = useState('');
-  const [referrer, setReferrer] = useState('');
+  const [feedback, setFeedback] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -27,18 +14,7 @@ const ContactSection: React.FC = () => {
     const formData = {
       name,
       email,
-      profession,
-      credentials,
-      company,
-      referrer,
-      _autoresponse: `Running apollo is hype you reached out to us. We will be in touch very soon :) Here is a copy of your submission: 
-      
-      Name: ${name}
-      Email: ${email}
-      Profession: ${profession}
-      Credentials: ${credentials}
-      Company: ${company}
-      Referrer: ${referrer}`
+      feedback
     };
 
     try {
@@ -55,10 +31,10 @@ const ContactSection: React.FC = () => {
 
       if (data) {
         console.log("Submission successful");
-        window.location.href = '/thankyou';
+        window.location.href = '/thank-you';
       }
     } catch (error) {
-      console.log(error);
+      console.error("Submission failed", error);
       window.location.href = '/error';
     } finally {
       setIsLoading(false);
@@ -66,130 +42,73 @@ const ContactSection: React.FC = () => {
   };
 
   return (
-    <>
-      <style jsx global>{`
-        body {
-          background-color: #5a3825;
-          margin: 0;
-        }
-      `}</style>
-      <div className="min-h-screen text-[#f4eae4]">
-        {isLoading && (
-          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#301f13] bg-opacity-75">
-            <div className="bg-[#301f13] rounded-lg shadow-lg p-8">
-              <h3 className="text-2xl font-semibold mb-4 text-[#f4eae4]">Your submission is being processed and you will be redirected once we confirm a successful submission.</h3>
-            </div>
+    <div className="min-h-screen bg-[#5a3825] text-[#f4eae4] flex flex-col justify-center">
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#301f13] bg-opacity-75">
+          <div className="bg-[#301f13] rounded-lg shadow-lg p-8">
+            <h3 className="text-2xl font-semibold mb-4 text-[#f4eae4]">Your submission is being processed and you will be redirected once we confirm a successful submission.</h3>
           </div>
-        )}
-        <section className="px-4 mt-8 mb-4 flex flex-col rounded-2xl items-center justify-center bg-[#422b1e] text-[#f4eae4] md:mt-10 md:px-0 md:max-w-lg md:mx-auto">
-          <div className="w-full">
-            <p className="text-base sm:text-lg m-4 text-center">
-              Running Apollo is a health protocol
-            </p>
-          </div>
-          <div className="w-full">
-            <p className="text-base sm:text-lg m-4 text-center">
-              We&apos;re on a mission to help others improve their foot health so they can lead happier lives.
-            </p>
-          </div>
-          <div className="w-full">
-            <p className="text-base sm:text-lg m-4 text-center">
-              To formally endorse best practices, please submit the form below:
-            </p>
-          </div>
-        </section>
+        </div>
+      )}
+      <section className="px-4 py-4 m-12 flex flex-col rounded-2xl items-center justify-center bg-[#422b1e] text-[#f4eae4] md:py-8 md:px-0 md:max-w-lg md:mx-auto">
+        <div className="w-full">
+          <p className="text-base sm:text-lg m-2 text-center">
+            We value your feedback. Please fill out the form below to share your thoughts with us.
+          </p>
+        </div>
+      </section>
 
-        <section className="px-4 mb-4 flex flex-col items-center justify-center min-h-screen bg-[#5a3825] text-[#f4eae4]">
-          <form
-            onSubmit={handleSubmit}
-            className="max-w-md sm:max-w-lg mx-auto w-full flex flex-col space-y-6 sm:space-y-4"
+      <section className="px-4 py-4 flex flex-col items-center justify-center bg-[#5a3825] text-[#f4eae4]">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md sm:max-w-lg mx-auto w-full flex flex-col space-y-4"
+        >
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Email (not made public, used to verify)</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Feedback</label>
+            <textarea
+              id="feedback"
+              name="feedback"
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              rows={4}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-1/2 mx-auto mb-4 bg-[#422b1e] hover:bg-black text-[#f4eae4] py-2 px-6 rounded-full"
           >
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Email (not made public, used to verify)</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Profession</label>
-              <input
-                type="text"
-                id="profession"
-                name="profession"
-                value={profession}
-                onChange={(e) => setProfession(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Credentials</label>
-              <input
-                type="text"
-                id="credentials"
-                name="credentials"
-                value={credentials}
-                onChange={(e) => setCredentials(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Company</label>
-              <input
-                type="text"
-                id="company"
-                name="company"
-                value={company}
-                onChange={(e) => setCompany(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Who sent you here?</label>
-              <select
-                value={referrer}
-                onChange={(e) => setReferrer(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                required
-              >
-                <option value="">Select an option</option>
-                <option value="health professional">Health professional</option>
-                <option value="university">University</option>
-                <option value="the foot collective">The Foot Collective</option>
-                <option value="other">Other</option>
-                <option value="Google Search">Google Search</option>
-                <option value="Professional Association">Professional Association</option>
-                <option value="Patient">Patient</option>
-              </select>
-            </div>
-            <button
-              type="submit"
-              className="w-1/2 mx-auto mb-8 bg-[#422b1e] hover:bg-black text-[#f4eae4] py-2 px-6 rounded-full"
-            >
-              Submit
-            </button>
-          </form>
-        </section>
-      </div>
-    </>
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
   );
 };
 
-export default ContactSection;
+export default FeedbackPage;
--- src/app/feedback/page.tsx
+++ src/app/feedback/page.tsx
@@ -1,16 +1,7 @@
 'use client';
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 
 const FeedbackPage: React.FC = () => {
-  const hasLogged = useRef(false);
-
-  useEffect(() => {
-    if (!hasLogged.current) {
-      console.log('Want a website like this? Visit https://magicwebstore.xyz/index.html?pubkey=02b1a6c208420d3eb5a625aa8c79689e1dd4ea94f82286d06e1df7a05e2c3a482f&relays=%5B%22wss://nostrue.com%22,%22wss://relayable.org%22,%22wss://nostr.oxtr.dev%22%5D');
-      hasLogged.current = true;
-    }
-  }, []);
-
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [feedback, setFeedback] = useState('');
@@ -23,12 +14,7 @@ const FeedbackPage: React.FC = () => {
     const formData = {
       name,
       email,
-      feedback,
-      _autoresponse: `Thank you for your feedback! Here is a copy of your submission: 
-      
-      Name: ${name}
-      Email: ${email}
-      Feedback: ${feedback}`
+      feedback
     };
 
     try {
@@ -56,80 +42,72 @@ const FeedbackPage: React.FC = () => {
   };
 
   return (
-    <>
-      <style jsx global>{`
-        body {
-          background-color: #5a3825;
-          margin: 0;
-        }
-      `}</style>
-      <div className="min-h-screen text-[#f4eae4]">
-        {isLoading && (
-          <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#301f13] bg-opacity-75">
-            <div className="bg-[#301f13] rounded-lg shadow-lg p-8">
-              <h3 className="text-2xl font-semibold mb-4 text-[#f4eae4]">Your submission is being processed and you will be redirected once we confirm a successful submission.</h3>
-            </div>
+    <div className="min-h-screen bg-[#5a3825] text-[#f4eae4] flex flex-col justify-center">
+      {isLoading && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-[#301f13] bg-opacity-75">
+          <div className="bg-[#301f13] rounded-lg shadow-lg p-8">
+            <h3 className="text-2xl font-semibold mb-4 text-[#f4eae4]">Your submission is being processed and you will be redirected once we confirm a successful submission.</h3>
           </div>
-        )}
-        <section className="px-4 mt-8 mb-4 flex flex-col rounded-2xl items-center justify-center bg-[#422b1e] text-[#f4eae4] md:mt-10 md:px-0 md:max-w-lg md:mx-auto">
-          <div className="w-full">
-            <p className="text-base sm:text-lg m-4 text-center">
-              We value your feedback. Please fill out the form below to share your thoughts with us.
-            </p>
-          </div>
-        </section>
+        </div>
+      )}
+      <section className="px-4 py-4 m-12 flex flex-col rounded-2xl items-center justify-center bg-[#422b1e] text-[#f4eae4] md:py-8 md:px-0 md:max-w-lg md:mx-auto">
+        <div className="w-full">
+          <p className="text-base sm:text-lg m-2 text-center">
+            We value your feedback. Please fill out the form below to share your thoughts with us.
+          </p>
+        </div>
+      </section>
 
-        <section className="px-4 mb-4 flex flex-col items-center justify-center min-h-screen bg-[#5a3825] text-[#f4eae4]">
-          <form
-            onSubmit={handleSubmit}
-            className="max-w-md sm:max-w-lg mx-auto w-full flex flex-col space-y-6 sm:space-y-4"
+      <section className="px-4 py-4 flex flex-col items-center justify-center bg-[#5a3825] text-[#f4eae4]">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md sm:max-w-lg mx-auto w-full flex flex-col space-y-4"
+        >
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Email (not made public, used to verify)</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              required
+            />
+          </div>
+          <div>
+            <label className="text-lg text-[#f4eae4] font-medium">Feedback</label>
+            <textarea
+              id="feedback"
+              name="feedback"
+              value={feedback}
+              onChange={(e) => setFeedback(e.target.value)}
+              className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
+              rows={4}
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-1/2 mx-auto mb-4 bg-[#422b1e] hover:bg-black text-[#f4eae4] py-2 px-6 rounded-full"
           >
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Name</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Email (not made public, used to verify)</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                required
-              />
-            </div>
-            <div>
-              <label className="text-lg text-[#f4eae4] font-medium">Feedback</label>
-              <textarea
-                id="feedback"
-                name="feedback"
-                value={feedback}
-                onChange={(e) => setFeedback(e.target.value)}
-                className="w-full border rounded px-4 py-2 text-black bg-[#f4eae4]"
-                rows={4} // Corrected to be a number
-                required
-              />
-            </div>
-            <button
-              type="submit"
-              className="w-1/2 mx-auto mb-8 bg-[#422b1e] hover:bg-black text-[#f4eae4] py-2 px-6 rounded-full"
-            >
-              Submit
-            </button>
-          </form>
-        </section>
-      </div>
-    </>
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
   );
 };
 
